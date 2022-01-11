74fc29e4d339da7dc008034967fb7ffeaa4c1bef
Shail Gor <shail.gor@smartsensesolutions.com>
2022-01-11 12:04:26 +0530

    Add variablr playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+//     name: "xyz",
+//     age: 21
+// };
+
+// function updatePerson (obj){
+//     // obj = {
+//     //     name: 'xyz',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 88];
+
+function addGrades (gradesArr){
+    grades.push(55);
+    debugger;
+    
+    // gradesArr = [12, 33, 85];
+}
+
+addGrades(grades);
+console.log(grades);
