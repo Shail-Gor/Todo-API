221270fa41ed7c9ff13170abeab5d35ea4cc9ec3
Shail Gor <shail.gor@smartsensesolutions.com>
2022-01-11 18:08:50 +0530

    Convert Get /todos to sequelize

--- server.js
+++ server.js
@@ -17,22 +17,27 @@ app.get('/', function (req, res) {
 
 // GET /todo's
 app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filterTodos = todos;
+    var query = req.query;
+    var where = {};
 
-    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true' ){
-        filterTodos = _.where(filterTodos, {completed: true});
-    }else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filterTodos = _.where(filterTodos, {completed: false});
+    if(query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    }else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
+
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like: '%' + query.q + '%'
+        };
+    }
+
+    db.todo.findAll({where: where}).then(function (todos) {
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
+    });
     
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
-        filterTodos = _.filter(filterTodos, function (todo){
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
-    }
-
-    res.json(filterTodos);
 });
 
 // GET /todo's/:id
