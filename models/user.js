4d8dc4a765720ecb7665927f2823b8de503ae7aa
Shail Gor <shail.gor@smartsensesolutions.com>
2022-01-12 15:17:22 +0530

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const { user } = require("../db");
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
 
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('user', {
@@ -10,19 +12,39 @@ module.exports = function (sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNullL: false,
             validate: {
                 len: [7, 100]
+            },
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword =bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
-            hooks: {
-            beforeValidate: function (users, options) {
+        hooks: {
+            beforeValidate: function (user, options) {
                 if (typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();
                 }
+            }
+        },
+        instanceMethods:{
+            toPublicJSON: function () {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
             }
         }
     });
